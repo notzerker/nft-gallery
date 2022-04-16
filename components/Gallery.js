ce53aeb50cd7f14af2759020589aa81dc0d653df
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-04-16 01:29:43 -0700

    updated card dialog

--- components/Gallery.js
+++ components/Gallery.js
@@ -6,6 +6,10 @@ import { CgProfile } from "react-icons/cg";
 import Item from "./Item";
 import { useRouter } from "next/router";
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import { addRequestMeta } from "next/dist/server/request-meta";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import Link from "next/link";
+import { IoOpenOutline } from "react-icons/io5";
 
 const Gallery = () => {
   const [url, setUrl] = useState();
@@ -13,6 +17,7 @@ const Gallery = () => {
   const [image, setImage] = useState();
   const [names, setNames] = useState([]);
   const [result, setResult] = useState([]);
+  const [addr, setAddr] = useState("");
 
   const apiKey = "SnGAV1KNt2faUFkP0C1d-TjoBcQB1HiU";
 
@@ -25,11 +30,30 @@ const Gallery = () => {
 
   useEffect(() => {
     if (address) {
+      const addressLength = address.length;
+      //add else if third case where user inputs .eth
+      if (addressLength == 42) {
+        const truncateAddress =
+          address.substring(0, 4) +
+          "..." +
+          address.substring(addressLength - 4, addressLength);
+        setAddr(truncateAddress);
+      } else {
+        address = address + ".eth";
+        setAddr(address);
+      }
+
       async function getNFTs(addr) {
-        const nfts = await web3.alchemy.getNfts({
-          owner: addr,
-        });
-        setResult(nfts.ownedNfts);
+        await web3.alchemy
+          .getNfts({
+            owner: addr,
+          })
+          .then((res) => {
+            setResult(res.ownedNfts);
+          })
+          .catch((error) => {
+            alert(error);
+          });
       }
       getNFTs(address);
     }
@@ -45,74 +69,83 @@ const Gallery = () => {
   });
 
   return (
-    <>
-      <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-24 py-24">
-        <div className=" flex flex-row items-center justify-center space-x-16">
-          <div className="relative flex flex-col items-center justify-center space-y-4 rounded-xl bg-dark p-16 shadow-xl">
-            <div className="absolute top-4 left-4 p-2">
-              <CgProfile className="cursor-pointer text-xl text-gray hover:text-white" />
-            </div>
-            <div className="absolute top-0 right-4 rounded-xl p-2">
-              <BsThreeDots className="cursor-pointer text-gray hover:text-white" />
-            </div>
-            <h1 className="w-80 truncate text-center text-6xl font-extrabold text-white">
-              {address}
-            </h1>
-            <div className="flex cursor-pointer flex-row items-center justify-center space-x-2 text-center text-gray hover:text-white">
-              <FaEthereum />
-              <p className="w-24 truncate">{address}</p>
-            </div>
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-center py-24">
+      <Link href="/">
+        <motion.a
+          className="absolute top-16 left-16 cursor-pointer rounded-xl bg-dark p-4 text-2xl text-light shadow-xl hover:text-white"
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 1 }}
+        >
+          <IoIosArrowRoundBack />
+        </motion.a>
+      </Link>
+      <div className=" mb-24 flex flex-row items-center justify-center space-x-24">
+        <div className="relative flex flex-col items-center justify-center space-y-4 rounded-xl bg-dark p-16 shadow-xl">
+          <div className="absolute top-4 left-4 p-2">
+            <CgProfile className="cursor-pointer text-xl text-gray hover:text-white" />
           </div>
-          <div className="before:broder-white relative  flex h-full flex-row space-x-12 p-8 pr-16 before:absolute before:bottom-0 before:right-0 before:h-4 before:w-4 before:border-b-[1px] before:border-r-[1px] after:absolute after:top-0 after:right-0 after:h-4 after:w-4 after:border-t-[1px] after:border-r-[1px]">
-            <div className="flex flex-col space-y-2">
-              <p className="text-sm uppercase tracking-widest text-gray">
-                Items
-              </p>
-              <h1 className="text-8xl font-extrabold">{result.length}</h1>
-              <p className="text-light">Number of individual ERC721 tokens.</p>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <p className="text-sm uppercase tracking-widest text-gray">
-                Collections
-              </p>
-              <h1 className="text-8xl font-extrabold">{collections.length}</h1>
-              <p className="text-light">Number of collections.</p>
-            </div>
+          <div className="absolute top-0 right-4 rounded-xl p-2">
+            <BsThreeDots className="cursor-pointer text-gray hover:text-white" />
+          </div>
+          <h1 className="text-center text-6xl font-extrabold text-white">
+            {addr}
+          </h1>
+          <a
+            className="flex cursor-pointer flex-row items-center justify-center space-x-2 text-center text-gray hover:text-white"
+            href={"https://etherscan.io/address/" + addr}
+            target="_blank"
+          >
+            <FaEthereum />
+            <p className="">{addr}</p>
+            <IoOpenOutline />
+          </a>
+        </div>
+        <div className="relative flex h-full flex-row space-x-12">
+          <div className="flex flex-col space-y-2">
+            <p className="text-sm uppercase tracking-widest text-gray">Items</p>
+            <h1 className="text-8xl font-extrabold">{result.length}</h1>
+            <p className="text-light">Number of individual ERC721 tokens.</p>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <p className="text-sm uppercase tracking-widest text-gray">
+              Collections
+            </p>
+            <h1 className="text-8xl font-extrabold">{collections.length}</h1>
+            <p className="text-light">Number of collections.</p>
           </div>
         </div>
-        <div className=" grid w-full grid-cols-4 gap-8 px-28">
-          {result.map((data) => {
-            var img = "";
-            var valid = true;
-            if (data.media[0].gateway == "") {
-              img = data.metadata.image_url;
-            } else {
-              img = data.media[0].gateway;
-            }
-
-            if (data.title == "") {
-              valid = false;
-            }
-
-            return valid ? (
-              <Item
-                name={data.title}
-                tokenId={data.token_id}
-                img={img}
-                desc={data.description}
-                attr={data.metadata.attributes}
-              />
-            ) : (
-              ""
-            );
-          })}
-        </div>
-        <div className="flex flex-row items-center justify-center space-x-24"></div>
       </div>
-      {/* <div className="fixed top-0 select-none backdrop-blur-3xl">
-        <div className="rounded-full " />
-      </div> */}
-    </>
+      <div className=" grid w-full grid-cols-4 gap-8 px-28">
+        {result.map((data) => {
+          var img = "";
+          var valid = true;
+          if (data.media[0].gateway == "") {
+            img = data.metadata.image_url;
+          } else {
+            img = data.media[0].gateway;
+          }
+
+          if (data.title == "") {
+            valid = false;
+          }
+
+          return valid ? (
+            <Item
+              name={data.title}
+              tokenId={data.id.tokenId}
+              tokenStd={data.id.tokenMetadata.tokenType}
+              img={img}
+              desc={data.description}
+              attr={data.metadata.attributes}
+              contract={data.contract.address}
+            />
+          ) : (
+            ""
+          );
+        })}
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-24"></div>
+    </div>
   );
 };
 
--- components/Item.js
+++ components/Item.js
@@ -2,14 +2,31 @@ import { motion } from "framer-motion";
 import React from "react";
 import Image from "next/image";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { IoOpenOutline } from "react-icons/io5";
 
-const Item = ({ name, tokenId, img, desc, attr }) => {
+const Item = ({ name, tokenId, img, desc, attr, contract, tokenStd }) => {
+  const truncateAddress = (addr) => {
+    const addressLength = addr.length;
+    const truncateAddr =
+      addr.substring(0, 4) +
+      "..." +
+      addr.substring(addressLength - 4, addressLength);
+    return truncateAddr;
+  };
+
+  const contractHanlder = () => {};
+
+  const variants = {
+    visible: { scale: 1, x: "-50%", y: "-50%" },
+    hidden: { scale: 0.9, x: "-50%", y: "-50%" },
+  };
+
   return (
     <DialogPrimitive.Root>
       <DialogPrimitive.Trigger>
         <motion.div
-          className="flex h-fit w-72 cursor-pointer select-none flex-col space-y-4 overflow-hidden rounded-xl bg-dark p-8 shadow-xl hover:scale-105"
-          whileHover={{ scale: 1.05 }}
+          className="flex h-fit w-72 cursor-pointer select-none flex-col overflow-hidden rounded-xl border border-light/20 bg-dark shadow-xl"
+          whileHover={{ scale: 1.03 }}
           whileTap={{ scale: 1 }}
         >
           <img
@@ -21,54 +38,124 @@ const Item = ({ name, tokenId, img, desc, attr }) => {
               currentTarget.oneError = null;
               currentTarget.src = "./assets/soju.jpeg";
             }}
-            className="h-56 w-56 rounded-lg object-cover"
+            className="rounded-y-xl aspect-square w-full"
           />
-          <div>
-            <p className="w-full truncate text-left text-lg font-extrabold text-white">
-              {name}
-            </p>
-            <p className="w-full truncate text-sm text-white">{tokenId}</p>
+          <div className="w-full p-4">
+            <p className="w-full truncate text-left text-white">{name}</p>
           </div>
         </motion.div>
       </DialogPrimitive.Trigger>
       <DialogPrimitive.Portal className="flex h-screen w-full items-center justify-center">
         <DialogPrimitive.Overlay className="fixed inset-0 bg-dark/80" />
-        <DialogPrimitive.Content className=" fixed left-1/2 top-1/2 flex h-[30rem] w-fit -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded-xl border border-light/50 bg-dark shadow-xl">
-          <img src={img} className="w-54 h-54 m-8 rounded-lg object-cover" />
-          <div className="flex h-fit flex-col items-start justify-center overflow-y-scroll p-8 px-8">
-            <h1 className="mb-6 inline text-left text-4xl font-extrabold text-white">
-              {name}
-            </h1>
-            {desc ? (
-              <>
-                <p className="mb-2 text-sm uppercase  tracking-widest text-gray">
-                  Description
-                </p>
-                <p className="w-[28rem] text-light">{desc}</p>
-              </>
-            ) : (
-              ""
-            )}
-            <p className="mt-6 mb-2 text-sm uppercase  tracking-widest text-gray">
-              Attributes
-            </p>
-            {attr ? (
-              <div className="grid w-full grid-cols-2 gap-4">
-                {attr.map((x) => {
-                  return (
-                    <div className="flex h-16 w-full flex-col items-start justify-center rounded-xl border-[1px] border-light/50 px-4">
-                      <p className="text-xs uppercase tracking-wider text-light">
-                        {x.trait_type}
-                      </p>
-                      <p className="text-sm">{x.value}</p>
+        <DialogPrimitive.Content>
+          <motion.div
+            className="fixed left-1/2 top-1/2 grid w-11/12 max-w-[72rem] grid-cols-12 gap-x-10 rounded-xl border border-light/20 bg-dark shadow-xl"
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+          >
+            <div className="col-span-6 aspect-square">
+              <img
+                src={img}
+                className="m-8 aspect-square w-full rounded-xl"
+                onError={({ currentTarget }) => {
+                  currentTarget.oneError = null;
+                  currentTarget.src = "./assets/soju.jpeg";
+                }}
+              />
+            </div>
+            <div className="col-span-6 h-full max-h-[37rem] overflow-scroll py-12 px-8">
+              <div className="flex h-fit flex-col items-start justify-center overflow-y-scroll">
+                <h1 className="mb-4 inline w-full text-left text-4xl font-extrabold tracking-tighter text-white ">
+                  {name}
+                </h1>
+                <div className=" mb-4 flex w-full space-x-4">
+                  <div>
+                    <Image
+                      src="/assets/opensea.svg"
+                      height={40}
+                      width={40}
+                      className="cursor-pointer shadow-xl"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="/assets/looksrare1.svg"
+                      height={40}
+                      width={40}
+                      className="cursor-pointer shadow-xl"
+                    />
+                  </div>
+                </div>
+                <div className="grid w-full grid-cols-3 gap-x-4">
+                  <div>
+                    <p className="mb-2 text-sm uppercase tracking-widest text-gray">
+                      Creator
+                    </p>
+                    <a
+                      className="mb-10 flex w-full cursor-pointer items-center text-light hover:text-white"
+                      href={"https://etherscan.io/address/" + contract}
+                      target="_blank"
+                    >
+                      <p className="mr-2">{truncateAddress(contract)}</p>
+                      <IoOpenOutline className="mb-1" />
+                    </a>
+                  </div>
+                  <div>
+                    <p className="mb-2 text-sm uppercase tracking-widest text-gray">
+                      Token ID
+                    </p>
+                    <p className="mb-10 flex w-32 items-center overflow-hidden text-light ">
+                      {parseInt(tokenId, 16).toLocaleString("fullwide", {
+                        useGrouping: false,
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="mb-2 text-sm uppercase tracking-widest text-gray">
+                      Token Standard
+                    </p>
+                    <p className="mb-10 flex w-full items-center text-light ">
+                      {tokenStd}
+                    </p>
+                  </div>
+                </div>
+                {desc ? (
+                  <>
+                    <p className="mb-2 text-sm uppercase tracking-widest text-gray">
+                      Description
+                    </p>
+                    <p className="mb-10 w-full text-light">{desc}</p>
+                  </>
+                ) : (
+                  ""
+                )}
+                {typeof attr !== "undefined" ? (
+                  <>
+                    <p className="mb-2 text-sm uppercase tracking-widest text-gray">
+                      Attributes
+                    </p>
+                    <div className="grid w-full grid-cols-2 gap-3">
+                      {attr.map((x) => {
+                        return (
+                          <div className="flex h-16 w-full flex-col items-start justify-center rounded-xl border-[1px] border-light/50 px-4">
+                            <p className="text-xs uppercase tracking-wider text-light">
+                              {x.trait_type}
+                            </p>
+                            <p className="w-full truncate text-sm font-extrabold">
+                              {x.value}
+                            </p>
+                          </div>
+                        );
+                      })}
                     </div>
-                  );
-                })}
+                  </>
+                ) : (
+                  ""
+                )}
               </div>
-            ) : (
-              ""
-            )}
-          </div>
+            </div>
+          </motion.div>
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>
     </DialogPrimitive.Root>
