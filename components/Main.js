128f74d62da83284d06474f386097a3b7dcf14c7
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-05-02 14:07:24 -0700

    hover effects

--- components/Main.js
+++ components/Main.js
@@ -73,19 +73,18 @@ const Main = () => {
           <div className="mt-20 flex flex-row space-x-6">
             <div className="group flex w-[22rem] cursor-pointer flex-col items-start justify-center rounded-xl bg-dark p-8 text-xs">
               <p className="uppercase tracking-wider text-gray">Connect</p>
-              <h2 className="mt-16 text-3xl font-extrabold tracking-tighter">
+              <h2 className="mt-16 text-3xl font-extrabold tracking-tighter text-white">
                 Connect
               </h2>
-              <p className="mt-4 text-sm">
+              <p className="mt-4 text-sm text-white">
                 Brings creators, collectors, and artists together to enjoy
                 blockchain art.
               </p>
               <svg
                 width={26}
                 height={24}
-                fill="none"
+                className="mt-16 transition duration-200 ease-linear group-hover:scale-105"
                 xmlns="http://www.w3.org/2000/svg"
-                className="mt-16"
               >
                 <mask id="a" className="fill-gray group-hover:fill-white">
                   <path
@@ -98,7 +97,7 @@ const Main = () => {
                   fillRule="evenodd"
                   clipRule="evenodd"
                   d="M14.5 4.665A2.5 2.5 0 0 0 13.25 0 2.5 2.5 0 0 0 12 4.665L3.83 18.817a2.5 2.5 0 1 0 1.92 2.433h15a2.5 2.5 0 1 0 1.92-2.433L14.5 4.665Zm-1.53.32a2.524 2.524 0 0 0 .56 0l8.231 14.257a2.509 2.509 0 0 0-.803 1.008H5.542a2.509 2.509 0 0 0-.803-1.008L12.97 4.985Z"
-                  fill="#171717"
+                  className="fill-dark "
                 />
                 <path
                   d="M14.5 4.665 14 3.8l-.865.5.5.865.865-.5Zm-2.5 0 .866.5.5-.865-.865-.5-.501.865ZM3.83 18.817l-.232.973.725.172.372-.645-.866-.5Zm1.92 2.433v-1h-1v1h1Zm15 0h1v-1h-1v1Zm1.92-2.433-.865.5.372.645.725-.171-.231-.974Zm-9.7-13.832.11-.994-.649-.073-.327.567.866.5Zm.56 0 .866-.5-.327-.567-.65.073.11.994Zm8.231 14.257.596.802.716-.53-.446-.772-.866.5Zm-.803 1.008v1h.654l.262-.6-.916-.4Zm-15.416 0-.916.4.262.6h.654v-1Zm-.803-1.008-.866-.5-.446.771.716.531.596-.802ZM14.75 2.5c0 .554-.3 1.039-.75 1.3L15 5.53A3.499 3.499 0 0 0 16.75 2.5h-2ZM13.25 1a1.5 1.5 0 0 1 1.5 1.5h2a3.5 3.5 0 0 0-3.5-3.5v2Zm-1.5 1.5a1.5 1.5 0 0 1 1.5-1.5v-2a3.5 3.5 0 0 0-3.5 3.5h2Zm.75 1.3c-.45-.261-.75-.746-.75-1.3h-2c0 1.297.706 2.427 1.749 3.031L12.501 3.8Zm-1.366.365-8.17 14.152 1.731 1 8.17-14.152-1.731-1ZM4.06 17.845a3.506 3.506 0 0 0-.81-.095v2c.121 0 .238.014.348.04l.462-1.946Zm-.81-.095a3.5 3.5 0 0 0-3.5 3.5h2a1.5 1.5 0 0 1 1.5-1.5v-2Zm-3.5 3.5a3.5 3.5 0 0 0 3.5 3.5v-2a1.5 1.5 0 0 1-1.5-1.5h-2Zm3.5 3.5a3.5 3.5 0 0 0 3.5-3.5h-2a1.5 1.5 0 0 1-1.5 1.5v2Zm2.5-2.5h15v-2h-15v2Zm14-1a3.5 3.5 0 0 0 3.5 3.5v-2a1.5 1.5 0 0 1-1.5-1.5h-2Zm3.5 3.5a3.5 3.5 0 0 0 3.5-3.5h-2a1.5 1.5 0 0 1-1.5 1.5v2Zm3.5-3.5a3.5 3.5 0 0 0-3.5-3.5v2a1.5 1.5 0 0 1 1.5 1.5h2Zm-3.5-3.5c-.278 0-.549.033-.81.095l.462 1.946c.11-.027.227-.041.348-.041v-2Zm.287.567-8.17-14.152-1.733 1 8.17 14.152 1.733-1ZM13.25 4c-.057 0-.114-.003-.17-.01l-.22 1.988c.128.015.258.022.39.022V4Zm.17-.01c-.056.007-.113.01-.17.01v2c.132 0 .262-.007.39-.022l-.22-1.987Zm9.207 14.752L14.396 4.485l-1.732 1 8.231 14.257 1.732-1Zm-.753 1.908c.106-.24.273-.45.483-.605l-1.192-1.606a3.51 3.51 0 0 0-1.123 1.41l1.832.801Zm-16.332.6h15.416v-2H5.542v2Zm-1.4-1.206c.21.157.378.366.484.606l1.832-.8a3.51 3.51 0 0 0-1.123-1.411l-1.192 1.605Zm7.962-15.56L3.873 18.743l1.732 1 8.231-14.257-1.732-1Z"
@@ -120,7 +119,7 @@ const Main = () => {
                 height={24}
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className="mt-16"
+                className="mt-16 transition duration-200 ease-linear group-hover:scale-105"
               >
                 <path
                   fillRule="evenodd"
@@ -144,7 +143,7 @@ const Main = () => {
                 height={24}
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className="mt-16 stroke-gray group-hover:stroke-white"
+                className="mt-16 stroke-gray transition duration-200 ease-linear group-hover:scale-105 group-hover:stroke-white"
               >
                 <path
                   d="M24.75 11.364c0 .366-.262.77-.872 1.183-.602.407-1.49.783-2.608 1.102-2.234.639-5.335 1.037-8.77 1.037-3.435 0-6.536-.399-8.77-1.037-1.119-.32-2.006-.695-2.608-1.102-.61-.412-.872-.817-.872-1.183 0-.366.262-.77.872-1.183.602-.406 1.49-.782 2.608-1.102 2.234-.638 5.335-1.036 8.77-1.036 3.435 0 6.536.398 8.77 1.036 1.119.32 2.006.696 2.608 1.102.61.413.872.817.872 1.183Z"
